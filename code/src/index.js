a6cdc3564343a5af8825e6ae5f58caf40b21a6ce
cbjorklund88 <clara.bjorklund@hotmail.com>
2018-10-02 13:47:06 +0200

    Some stretch-goals

--- code/src/index.js
+++ code/src/index.js
@@ -4,15 +4,50 @@ import "./index.css"
 
 class Clock extends React.Component {
 
+  state = {
+    date: new Date()
+  }
+
+  componentDidMount() {
+    this.startClock()
+  }
+
+  componentWillUnmount() {
+    this.stopClock()
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    })
+  }
+
+  stopClock= () => {
+    clearInterval(this.timerID)
+  }
+
+  startClock= () => {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    )
+  }
+
   render() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+      <div className="myClockPage">
+        <div className="myClock">
+          <h1>The Local Time Is:</h1>
+          <h2>{this.state.date.toLocaleTimeString()}</h2>
+          <div className="actionButtons">
+          <button onClick={this.stopClock}>Stop Clock</button>
+          <button onClick={this.startClock}>Start Clock</button>
+          </div>
+        </div>
       </div>
     )
   }
 
 }
 
-ReactDOM.render(<Clock date={new Date()} />, document.getElementById("root"))
+ReactDOM.render(<Clock />, document.getElementById("root"))
